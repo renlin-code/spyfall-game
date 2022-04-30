c3db28c6f02e70c676c944930c680157aa344d79
renlin-code <renelj1997@gmail.com>
2022-04-30 20:17:24 +0500

    Programa de contadores de match-param listo

--- script/main.js
+++ script/main.js
@@ -21,7 +21,7 @@ burgerMenu.addEventListener("click", showMenu);
 homeButton.addEventListener("click", hideMenu);    
 
 
-//SECTIONS ANIMATION
+//SECTIONS NAVIGATION ANIMATION
 const rulesButton = document.getElementById("rulesButton");
 const rulesSection = document.querySelector(".rules-section");
 const rulesCloseButton = document.getElementById("rulesCloseButton");
@@ -61,3 +61,129 @@ matchParamCloseButton.addEventListener("click", () => {
     changeSection(matchParamSection, mainSection);
     showSection(burgerMenu)
 });
+
+
+//PARAMETERS COUNTERS
+class Parameter {
+    constructor({
+        textId,
+        textInSingular,
+        textInPlural,
+        counterId,
+        minValue,
+        maxValue,
+        minusButtonId,
+        plusButtonId, 
+    })
+    {   
+        this.textId = textId;
+        this.textInSingular = textInSingular;
+        this.textInPlural = textInPlural;
+        this.counterId = counterId;
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+        this.minusButtonId = minusButtonId;
+        this.plusButtonId = plusButtonId;
+    }
+    decreaseCounter () {
+        let counter = document.getElementById(this.counterId)
+        let counterParsed = parseInt(counter.innerHTML);
+        let text = document.getElementById(this.textId);
+
+        if (counterParsed <= 2) {
+            text.innerHTML = this.textInSingular;
+        } else {
+            text.innerHTML = this.textInPlural;
+        }
+        if (counterParsed > this.minValue + 1) {
+            const minusButton = document.getElementById(this.minusButtonId);
+            minusButton.style.opacity = 1
+        } else {
+            const minusButton = document.getElementById(this.minusButtonId);
+            minusButton.style.opacity = 0.5 
+        }
+
+        if (counterParsed < this.maxValue + 1) {
+            const plusButton = document.getElementById(this.plusButtonId);
+            plusButton.style.opacity = 1
+        } else {
+            const plusButton = document.getElementById(this.plusButtonId);
+            plusButton.style.opacity = 0.5
+        }
+
+        if (counterParsed != this.minValue) {
+            counterParsed--;
+        }
+        counter.innerHTML = counterParsed;
+    }
+    increaseCounter () {
+        let counter = document.getElementById(this.counterId)
+        let counterParsed = parseInt(counter.innerHTML);
+        let text = document.getElementById(this.textId);
+
+        if (counterParsed >= 1) {
+            text.innerHTML = this.textInPlural;
+        } else {
+            text.innerHTML = this.textInSingular;
+        }
+        if (counterParsed > this.minValue - 1) {
+            const minusButton = document.getElementById(this.minusButtonId);
+            minusButton.style.opacity = 1
+        } else {
+            const minusButton = document.getElementById(this.minusButtonId);
+            minusButton.style.opacity = 0.5 
+        }
+        if (counterParsed < this.maxValue - 1) {
+            const plusButton = document.getElementById(this.plusButtonId);
+            plusButton.style.opacity = 1
+        } else {
+            const plusButton = document.getElementById(this.plusButtonId);
+            plusButton.style.opacity = 0.5
+        }
+        if (counterParsed != this.maxValue) {
+            counterParsed++;
+        }
+        counter.innerHTML = counterParsed;
+    }
+};
+
+const amountOfPlayers = new Parameter({
+    textId: "playersCounterText",
+    textInSingular: "PLAYER",
+    textInPlural: "PLAYERS",
+    counterId: "playersCounter",
+    minusButtonId: "playersMinusButton",
+    plusButtonId: "playersPlusButton",
+    minValue: 3,
+    maxValue: 10
+});
+const amountOfSpies = new Parameter({
+    textId: "spiesCounterText",
+    textInSingular: "SPY",
+    textInPlural: "SPIES",
+    counterId: "spiesCounter",
+    minusButtonId: "spiesMinusButton",
+    plusButtonId: "spiesPlusButton",
+    minValue: 1,
+    maxValue: 3
+});
+const amountOfTime = new Parameter({
+    textId: "timeCounterText",
+    textInSingular: "MINUTE",
+    textInPlural: "MINUTES",
+    counterId: "timeCounter",
+    minusButtonId: "timeMinusButton",
+    plusButtonId: "timePlusButton",
+    minValue: 5,
+    maxValue: 15
+});
+
+
+document.getElementById("playersMinusButton").addEventListener("click", () => {amountOfPlayers.decreaseCounter()});
+document.getElementById("playersPlusButton").addEventListener("click", () => {amountOfPlayers.increaseCounter()});
+
+document.getElementById("spiesMinusButton").addEventListener("click", () => {amountOfSpies.decreaseCounter()});
+document.getElementById("spiesPlusButton").addEventListener("click", () => {amountOfSpies.increaseCounter()});
+
+document.getElementById("timeMinusButton").addEventListener("click", () => {amountOfTime.decreaseCounter()});
+document.getElementById("timePlusButton").addEventListener("click", () => {amountOfTime.increaseCounter()});
